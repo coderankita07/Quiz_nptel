63847e1e8a13e92a57eb7a15756dbb3db5b29321
saurabhraj123 <me4saurabh4u@gmail.com>
2022-10-20 18:49:49 +0530

    Added notes link on about page

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import '../../styles/about.css'
+import React from "react";
+import "../../styles/about.css";
 
 const About = () => {
-    return ( <div className='about'>
-    <h1>About</h1>
-    <p>Revision Portal for Software Testing</p>
-    <p><i>- by Saurabh Raj</i></p>
-    <br />
-    <p><i>Email: me4saurabh4u@gmail.com</i></p>
-    <p><i>LinkedIn: <a href="https://www.linkedin.com/in/saurabhcu/" target="_blank">Link</a></i></p>
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <p>Revision Portal for Software Testing</p>
+      <p>
+        <i>- by Saurabh Raj</i>
+      </p>
+      <br />
+      <p>
+        <strong>Email: </strong> <a href="mailto: me4saurabh4u@gmail.com">me4saurabh4u@gmail.com</a>
+      </p>
+      <p>
+        <strong>
+          LinkedIn:{" "}
+        </strong>
+          <a href="https://www.linkedin.com/in/saurabhcu/" target="_blank">
+            Link
+          </a>
+      </p>
+      <br />
+      <p>
+          <strong>Chapterwise PDFs (Drive Link):{" "}</strong>
+          <a href="https://drive.google.com/drive/folders/1ZF8BbL012nj6dNoRVCAh0mNZP709-9DR" target="_blank">
+             Link
+          </a>{" "}
+      </p>
     </div>
-     );
-}
- 
+  );
+};
+
 export default About;
